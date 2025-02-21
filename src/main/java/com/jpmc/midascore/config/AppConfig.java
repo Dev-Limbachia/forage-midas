5a8f4a45dfbff317dd14f98fbc415e9e8761f02c
Dev-Limbachia <bplfamily21@gmail.com>
2025-02-21 20:15:46 +0000

    (~)

--- src/main/java/com/jpmc/midascore/config/AppConfig.java
+++ src/main/java/com/jpmc/midascore/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.jpmc.midascore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,5 +11,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(33400);
     }
 }
--- src/main/java/com/jpmc/midascore/foundation/Balance.java
+++ src/main/java/com/jpmc/midascore/foundation/Balance.java
@@ -4,13 +4,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Balance {
+    private String userId;
     private float amount;
 
     public Balance() {
     }
 
-    public Balance(float amount) {
+    public Balance(String userId, float amount) {
+        this.userId = userId;
         this.amount = amount;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public float getAmount() {
